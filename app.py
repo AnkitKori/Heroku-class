8660d939e423475e1d246f2f3142ecaa677a450d
AnkitKori <75736886+AnkitKori@users.noreply.github.com>
2021-06-25 13:59:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-import pickle
 import numpy as np
-from flask import Flask,render_template,request,jsonify
-
+from flask import Flask, request, jsonify, render_template
+import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index1.html')
-
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
-    for rending results on HTML GUI
+    For rendering results on HTML GUI
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
@@ -22,21 +20,8 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html',prediction_text='Employee Salary Should Be $ {}'.format(output))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    for direct API calls through request
-    '''    
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
-if __name__=='__main__':
-     app.run(debug=True)    
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
